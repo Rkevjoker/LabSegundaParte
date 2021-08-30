b0bd8873ffd13647b1de307900bd7a772ded1dff
Rkevjoker <88000934+Rkevjoker@users.noreply.github.com>
2021-08-30 16:49:59 -0600

    act3

--- LabSegundaParte/LabSegundaParte/Entidades/Persona.cs
+++ LabSegundaParte/LabSegundaParte/Entidades/Persona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabSegundaParte.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -42,6 +43,10 @@ namespace LabSegundaParte.Entidades
             this.Edad = edad;
             this.Peso = peso;
             this.Altura = altura;
+        }
+
+        public Persona()
+        {
         }
 
         public override string ToString()
--- LabSegundaParte/LabSegundaParte/Negocio/ClsPersona.cs
+++ LabSegundaParte/LabSegundaParte/Negocio/ClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LabSegundaParte.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,32 @@ namespace LabSegundaParte.Negocio
 {
     class ClsPersona
     {
+        Persona pers = new Persona();
+
+        public String calcularIMC()
+        {
+            double pro = pers.Peso / Math.Pow(pers.Altura, 2);
+
+            if (pro < 20)
+            {
+                return "el peso es ideal";
+            }
+            if (pro > 25 )
+            {
+                return "tiene sobre peso";
+            }
+            return "";
+        }
+
+        public String MayorDeEdad()
+        {
+            if (pers.Edad >= 18)
+            {
+                return "el usuario es mayor de edad";
+
+            }
+            else
+                return "el usuario es menor de edad";
+        }
     }
 }

0c27d97905e1f8654f27beffb8b570df1d813bc4
jeffer2020 <rc0113032021@unab.edu.sv>
2021-08-30 20:05:32 -0600

    repo.3

--- LabSegundaParte/LabSegundaParte/Program.cs
+++ LabSegundaParte/LabSegundaParte/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LabSegundaParte.Entidades;
+using LabSegundaParte.Negocio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,38 @@ namespace LabSegundaParte
 {
     class Program
     {
+       
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Escribe tu nombre: ");
+            String nombre = Console.ReadLine();
+
+            Console.WriteLine("Escribe cual es tu genero: ");
+            String sexo = Console.ReadLine();
+
+            Console.WriteLine("Escribe cual es tu edad: ");
+            int edad = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escribe cual es tu peso: ");
+            double peso = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Escribe cual es tu altura: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+
+
+            Persona persona = new Persona(nombre, sexo, edad, peso, altura);
+            Persona pers = new Persona(nombre, sexo, edad);
+
+            ClsPersona cls = new ClsPersona();
+            Console.WriteLine(cls.calcularIMC(pers));
+            Console.WriteLine(cls.MayorDeEdad());
+
+            Console.WriteLine(persona.ToString());
+            Console.ReadLine();
+
+
+
 
         }
     }
